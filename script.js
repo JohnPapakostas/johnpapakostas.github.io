68992ce8a719df81f3c6ecc46190ae1e3f18030f
JohnPapakostas <84929430+JohnPapakostas@users.noreply.github.com>
2025-04-16 17:02:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function copyBibtex(id) {
   const bib = document.getElementById(id).textContent;
   navigator.clipboard.writeText(bib).then(() => {
-    alert("BibTeX copied to clipboard!");
   });
 }
